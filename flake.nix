9bd7bae66ecad3e277fef891f0b95fe197b2b741
Goetz Markgraf <goetz.markgraf@codecentric.de>
2026-06-05 15:03:00 +0200

    initial

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  # Pfad zu deiner zentralen Datei anpassen!
+  inputs.my-base.url = "path:/Users/dragon/config-files/nix-config/flake-templates";
+
+  outputs = { my-base, ... }: my-base.lib.mkMacShell {
+    packages = pkgs: with pkgs; [
+      nodejs_24
+      opencode
+      pkg-config
+    ];
+  };
+}
